df496a29a7e9fd19005d48c3a7ae8fe73d312bb2
Lukas Micke <LukasMicke96@gmail.com>
2021-04-25 19:44:29 +0200

    Migrated to GCP Cloud SQL

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,39 +16,151 @@ import (
 
 func main() {
 
-	db, err := sql.Open(
-		"mysql",
-		"api:1234@tcp(127.0.0.1:3306)/diary",
-	)
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// if there is an error inserting, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// defer the close till after the main function has finished
-	// executing
-	defer db.Close()
-
-	r := mux.NewRouter()
-	r.HandleFunc("/entry/{id}", handlers.MakeGetEntry(db)).Methods("GET")
-	r.Handle("/entry", handlers.MakeCreateEntry(db)).Methods("POST")
-	r.Handle("/entry/{id}", handlers.MakeDeleteEntry(db)).Methods("DELETE")
-	r.Handle("/entry/{id}", handlers.MakeUpdateEntry(db)).Methods("PUT")
-	//http.Handle("/", r)
+	app := newApp()
 
 	srv := &http.Server{
-		Handler: r,
-		Addr:    "127.0.0.1:8000",
+		Handler: app.router,
+		Addr:    ":8080",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+type app struct {
+	router *mux.Router
+	db     *sql.DB
+}
+
+func newApp() *app {
+
+	r := mux.NewRouter()
+
+	app := &app{
+		router: r,
+	}
+
+	var err error
+
+	// If the optional DB_HOST environment variable is set, it contains
+	// the IP address and port number of a TCP connection pool to be created,
+	// such as "127.0.0.1:3306". If DB_HOST is not set, a Unix socket
+	// connection pool will be created instead.
+	if os.Getenv("DB_HOST") != "" {
+		app.db, err = initTCPConnectionPool()
+		if err != nil {
+			log.Fatalf("initTCPConnectionPool: unable to connect: %v", err)
+		}
+	} else {
+		app.db, err = initSocketConnectionPool()
+		if err != nil {
+			log.Fatalf("initSocketConnectionPool: unable to connect: %v", err)
+		}
+	}
+
+	// Create the votes table if it does not already exist.
+	if _, err = app.db.Exec(`CREATE TABLE IF NOT EXISTS votes
+	( id SERIAL NOT NULL, created_at datetime NOT NULL, updated_at datetime  NOT NULL,
+	candidate VARCHAR(6) NOT NULL, PRIMARY KEY (id) );`); err != nil {
+		log.Fatalf("DB.Exec: unable to create table: %s", err)
+	}
+
+	app.router.HandleFunc("/entry/{id}", handlers.MakeGetEntry(app.db)).Methods("GET")
+	app.router.Handle("/entry", handlers.MakeCreateEntry(app.db)).Methods("POST")
+	app.router.Handle("/entry/{id}", handlers.MakeDeleteEntry(app.db)).Methods("DELETE")
+	app.router.Handle("/entry/{id}", handlers.MakeUpdateEntry(app.db)).Methods("PUT")
+	return app
+}
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.\n", k)
+	}
+	return v
+}
+
+// initSocketConnectionPool initializes a Unix socket connection pool for
+// a Cloud SQL instance of SQL Server.
+func initSocketConnectionPool() (*sql.DB, error) {
+	// [START cloud_sql_mysql_databasesql_create_socket]
+	var (
+		dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
+		dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+	)
+
+	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	if !isSet {
+		socketDir = "/cloudsql"
+	}
+
+	var dbURI string
+	dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
+
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("mysql", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+
+	// [START_EXCLUDE]
+	configureConnectionPool(dbPool)
+	// [END_EXCLUDE]
+
+	return dbPool, nil
+	// [END cloud_sql_mysql_databasesql_create_socket]
+}
+
+// initTCPConnectionPool initializes a TCP connection pool for a Cloud SQL
+// instance of SQL Server.
+func initTCPConnectionPool() (*sql.DB, error) {
+	// [START cloud_sql_mysql_databasesql_create_tcp]
+	var (
+		dbUser    = mustGetenv("DB_USER") // e.g. 'my-db-user'
+		dbPwd     = mustGetenv("DB_PASS") // e.g. 'my-db-password'
+		dbTCPHost = mustGetenv("DB_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+		dbPort    = mustGetenv("DB_PORT") // e.g. '3306'
+		dbName    = mustGetenv("DB_NAME") // e.g. 'my-database'
+	)
+
+	var dbURI string
+	dbURI = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPwd, dbTCPHost, dbPort, dbName)
+
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("mysql", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+
+	// [START_EXCLUDE]
+	configureConnectionPool(dbPool)
+	// [END_EXCLUDE]
+
+	return dbPool, nil
+	// [END cloud_sql_mysql_databasesql_create_tcp]
+}
+
+// configureConnectionPool sets database connection pool properties.
+// For more information, see https://golang.org/pkg/database/sql
+func configureConnectionPool(dbPool *sql.DB) {
+	// [START cloud_sql_mysql_databasesql_limit]
+
+	// Set maximum number of connections in idle connection pool.
+	dbPool.SetMaxIdleConns(3)
+
+	// Set maximum number of open connections to the database.
+	dbPool.SetMaxOpenConns(5)
+
+	// [END cloud_sql_mysql_databasesql_limit]
+
+	// [START cloud_sql_mysql_databasesql_lifetime]
+
+	// Set Maximum time (in seconds) that a connection can remain open.
+	dbPool.SetConnMaxLifetime(1800)
+
+	// [END cloud_sql_mysql_databasesql_lifetime]
 }
